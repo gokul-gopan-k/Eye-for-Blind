57c2b672b8e08ac9625e17913878472dac428a85
gokul-gopan-k <gokul.gopan.k@gmail.com>
2024-10-16 16:35:00 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+git clone https://github.com/VarunGumma/IndicTransToolkit.git
+pip install gdown
+gdown --folder "https://drive.google.com/drive/folders/1yUQYZaTvslZ_2hHXUZrgFypTFR22L3bE"
